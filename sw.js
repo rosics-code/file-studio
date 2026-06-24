f8f18eae19169de86ff0df63d4d29ccf1fc8f248
John <roblox888i12345@gmail.com>
2026-06-24 04:35:20 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "image-studio-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
